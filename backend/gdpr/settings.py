92aa4ceb93e9214b3496298bdb81c0bf19c1e2ff
loispostula <lois@postu.la>
2018-04-04 17:16:46 +0200

    [enh] add local settings

--- backend/gdpr/settings.py
+++ backend/gdpr/settings.py
@@ -122,8 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # REST FRAMEWORK CONFIG
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
@@ -146,3 +144,13 @@ JWT_AUTH = {
 CORS_ORIGIN_WHITELIST = [
     'localhost:8080',
 ]
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
